2581ea38fd05440fea8d0e96f5d2424bf79f5d53
Andrey Pakhomov <pahomeathome@gmail.com>
2022-06-19 10:27:21 +0300

    percentage added

--- src/components/Vidget/Vidget.js
+++ src/components/Vidget/Vidget.js
@@ -12,23 +12,31 @@ class Vidget extends Component {
         this.setState(prevState => ({ [e.target.name]: prevState[e.target.name] + 1, }))
     };
 
-    countTotalFeedback = () => Object.values(this.state).reduce((prev, element) => prev + element,0)
-
+    countTotalFeedback = () => Object.values(this.state).reduce((prev, element) => prev + element, 0)
+    
+    countPositiveFeedbackPercentage = () => {
+        const total = this.countTotalFeedback();
+        if (total) {  
+            return Math.round(this.state.good / total * 100) + `%`;
+        }
+        return;
+    };
+    
     render() {
         const { good, neutral, bad } = this.state;
     
-        return <VidgetSection onChange={this.countTotalFeedback}>
+        return <VidgetSection>
 
             <Title>Please leave feedback</Title>
             <GreenButton name='good' onClick={(e) => this.handleIncrement(e)}>Good</GreenButton>
             <Button name='neutral' onClick={(e) => this.handleIncrement(e)}>Neutral</Button>
             <RedButton name='bad' onClick={(e) => this.handleIncrement(e)}>Bad</RedButton>
             <Title>Statistics</Title>
-            <GreenText>good:{good}</GreenText>
-            <Text>neutral:{neutral}</Text>
-            <RedText>bad:{bad}</RedText>
-            <Text onChange={this.countTotalFeedback}>Total:{this.countTotalFeedback()}</Text>
-            <Text>Positive feedback:</Text>
+            <GreenText>good: {good}</GreenText>
+            <Text>neutral: {neutral}</Text>
+            <RedText>bad: {bad}</RedText>
+            <Text>Total: {this.countTotalFeedback()}</Text>
+            <Text>Positive feedback: {this.countPositiveFeedbackPercentage()}</Text>
         
         </VidgetSection>;
     }
